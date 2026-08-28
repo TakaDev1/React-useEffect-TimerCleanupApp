088c3be6a7c90757d699736c888782b94c3a8a8c
Izumi Takayuki <r8quattrospider@gmail.com>
2026-08-28 13:19:16 +0900

    feat: Created DisplayCounter Component

--- src/components/DisplayCount.tsx
+++ src/components/DisplayCount.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const DisplayCount = ({ count }: { count: number }) => {
-  return <div>DisplayCount</div>;
+  return (
+    <div>
+      <p>Count: {count}</p>
+    </div>
+  );
 };
 
 export default DisplayCount;
