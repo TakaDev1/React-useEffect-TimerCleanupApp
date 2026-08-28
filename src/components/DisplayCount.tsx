71a30d585b4307da92e6abe43171631635e89bbf
Izumi Takayuki <r8quattrospider@gmail.com>
2026-08-28 14:16:37 +0900

    style: Adjustment Design

--- src/components/DisplayCount.tsx
+++ src/components/DisplayCount.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const DisplayCount = ({ count }: { count: number }) => {
   return (
-    <div>
+    <div className="bg-purple-900 text-white px-20 py-5">
       <p>Count: {count}</p>
     </div>
   );
